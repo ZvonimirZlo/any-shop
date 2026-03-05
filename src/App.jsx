c888927ae695be6f0deec54e8ea94e448b821fba
ZvonimirZlo <lovrez2@gmail.com>
2026-03-05 12:32:46 +0100

    Initializing new project Any shop

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+const App = () => {
+  return (
+    <>
+    <div className="hero flex align-center justify-evenly">
+    <h1 className="text-4xl">Hi!</h1>
+    <h1 className="text-4xl">Hi!</h1>
+    <button className="bg-sky-200 hover:bg-amber-200 ease-in-out rounded-2xl text-2xl">Save changes</button>
+    </div>
+    </>
+
+  )
+}
+
+export default App
